fe1feacddda09bebe8d237e3fbdf315679d58683
Michael Geers <michael@geers.tv>
2011-07-19 21:09:55 +0200

    removed empty bot profile layer

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,17 +2,6 @@
   head(function() {
     $('#guessesByGroup').guessesByGroup();
     $('#pointsBySeasonTable').pointsBySeasonTable();
-    $('#pointsBySeasonChart').pointsBySeasonChart();
-    return $('#guessesByGroup .google-visualization-table-table tbody tr td:first-child:not(.google-visualization-table-sorthdr)').live('click', function(e) {
-      var $layer, botName;
-      e.preventDefault();
-      botName = $(this).text();
-      $layer = $('<div />').attr('id', 'botProfileLayer');
-      $('#botProfileLayer').remove();
-      $('#overall').append($layer);
-      return $layer.profile({
-        botName: botName
-      });
-    });
+    return $('#pointsBySeasonChart').pointsBySeasonChart();
   });
 }).call(this);
